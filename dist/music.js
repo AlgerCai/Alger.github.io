8f3992951dd1a9774d79859c3687f183e98f2912
轻率的保罗 <1135637451@qq.com>
2020-07-12 11:20:54 +0800

    Site updated: 2020-07-12 11:20:53

---
+++ dist/music.js
@@ -0,0 +1,55 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+        {
+          name: 'JerryChen-夜空中最亮的星',
+          artist: '7killer',
+          url: 'http://music.163.com/song/media/outer/url?id=1376568671.mp3',
+          cover: 'http://r.photo.store.qq.com/psc?/V10MCVTZ1lsqm6/OgsY8p8GsL2M2s50.OYmenV92jr2ioRsn2GYQBhsaRd0tL0pwhBA0Ctua0yWYywX3trZtIs8N7PTKuWfs8AcvEh7kHmmU8UxAw9sqNrhiTQ!/r',
+        },
+        {
+          name: '一曲相思',
+          artist: '邓壬鑫',
+          url: 'http://music.163.com/song/media/outer/url?id=1342573857.mp3',
+          cover: 'http://r.photo.store.qq.com/psc?/V10MCVTZ1lsqm6/OgsY8p8GsL2M2s50.OYmegKw*Q*R0Q4kVhxeIehaAyOrxfHfQ9JSG4hKVzoRk.Z4smFzS4u00jmFImGB80KMZlMby3Ri2sQdT4L9JXz7KR0!/r',
+        },
+        {
+          name: 'Alan Walker-Faded',
+          artist: 'Hare Je',
+          url: 'http://music.163.com/song/media/outer/url?id=1364800914.mp3',
+          cover: 'http://r.photo.store.qq.com/psc?/V10MCVTZ1lsqm6/OgsY8p8GsL2M2s50.OYmepdxWAbQw4xEofFp2ReJkkjZeYLsAOAjIGipWohIEPH9HMar0gIWmynO1B0RqgSGCQHqWQ68gNNRIZHJgIx*pss!/r',
+        },
+        {
+          name: 'You',
+          artist: 'Approaching Nirvana',
+          url: 'http://music.163.com/song/media/outer/url?id=3932159.mp3',
+          cover: 'http://r.photo.store.qq.com/psc?/V10MCVTZ1lsqm6/OgsY8p8GsL2M2s50.OYmel3J4Ge3GNos8lzw7pTKEa3l.3Fuerx*0*1mFnVXtK4Ev7Qbi9IH9MfhBCK90ZoSL1LmK5rQVLyPEPzL58NBZxQ!/r',
+        },
+        {
+          name: 'いのちの名前',
+          artist: '広橋真紀子',
+          url: 'http://music.163.com/song/media/outer/url?id=578090.mp3',
+          cover: 'http://r.photo.store.qq.com/psc?/V10MCVTZ1lsqm6/OgsY8p8GsL2M2s50.OYmeiIE1MbeTvrkoA*P7xui3fNqTV75rA1UnnO7le5BI0*t.vI907WEtoqA3lgqt8IXcckIYNuxHc8E5rfCLpJgeF8!/r',
+        },
+        {
+          name: 'MELANCHOLY',
+          artist: 'White Cherry',
+          url: 'http://music.163.com/song/media/outer/url?id=1297742167.mp3',
+          cover: 'http://r.photo.store.qq.com/psc?/V10MCVTZ1lsqm6/OgsY8p8GsL2M2s50.OYmenhExYw7x2FwnrM1p4gvkzPxATqDzuPmacPG4IxVARVhbWHOgwk57VGqTxgn5bANY46fOVHQfI1VpAdSFk3vCRA!/r',
+        },
+        {
+          name: 'LOVE Theme from TIGA <M-2>',
+          artist: '矢野立美',
+          url: 'http://music.163.com/song/media/outer/url?id=4968365.mp3',
+          cover: 'http://r.photo.store.qq.com/psc?/V10MCVTZ1lsqm6/OgsY8p8GsL2M2s50.OYmehHOvpI.J26oh*HM68oCrtgRS0kqnBMqXgDnVp22H96wFeLgmTT*zd5qhI.Qec22znXMA.E0UsYxm0VNvuhrp.Y!/r',
+        },
+        {
+          name: '遠き呼び声の彼方へ <M-41,M-62A>',
+          artist: '矢野立美',
+          url: 'http://music.163.com/song/media/outer/url?id=4968374.mp3',
+          cover: 'http://r.photo.store.qq.com/psc?/V10MCVTZ1lsqm6/OgsY8p8GsL2M2s50.OYmehHOvpI.J26oh*HM68oCrtgRS0kqnBMqXgDnVp22H96wFeLgmTT*zd5qhI.Qec22znXMA.E0UsYxm0VNvuhrp.Y!/r',
+        }
+    ]
+});
